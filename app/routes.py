b2900f7519d8dd2b4e56abaf72a003acfc773fe6
Lyghtnox <nat.campan@gmail.com>
2020-10-06 23:18:11 +0200

    First login template

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,13 @@
+from flask import request, render_template
 from app import app
+
 
 @app.route('/', methods=['GET'])
 def index():
     return "PARTY TIME BOYZZZZ"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "GET":
+        return render_template('login.jinja2')
