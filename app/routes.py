a79af0fca6b6318d21a0a0a17709ede8ef3a4176
Lyghtnox <nat.campan@gmail.com>
2020-10-08 16:16:45 +0200

    Login should work

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import request, render_template
 from app import app
+from app.user import User
 
 
 @app.route('/', methods=['GET'])
@@ -11,3 +12,18 @@ def index():
 def login():
     if request.method == "GET":
         return render_template('login.jinja2')
+
+    elif request.method == "POST":
+        data = request.form.to_dict()
+        user = User.query.filter_by(email=data['email']).first()
+
+        if user is not None:
+            if user.verifyPassword(data['password']):
+                return "Logged in"
+
+        return render_template('login.jinja2')
+
+@app.route('/setup', methods=['GET'])
+def setup():
+    User.dbSetup()
+    return "Setup complete"
